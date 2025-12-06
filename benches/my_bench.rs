b40563f40dc6a993633f4b6135b84b877a9beca5
georgev93 <georgeavogt93@gmail.com>
2025-12-06 08:14:25 +0530

    Splitting out benchmarks

--- benches/my_bench.rs
+++ benches/my_bench.rs
@@ -1,9 +1,23 @@
-use aoc_2025_04::solve;
+use aoc_2025_04::{load_file, solve, solve_part1, solve_part2};
 use criterion::{Criterion, criterion_group, criterion_main};
 
 fn bench_solve(c: &mut Criterion) {
     c.bench_function("solve input.txt", |b| b.iter(|| solve("data/input.txt")));
 }
 
-criterion_group!(benches, bench_solve);
+fn bench_load(c: &mut Criterion) {
+    c.bench_function("input", |b| b.iter(|| load_file("data/input.txt")));
+}
+
+fn bench_solve_part1(c: &mut Criterion) {
+    let input_file = load_file("data/input.txt");
+    c.bench_function("input", move |b| b.iter(|| solve_part1(input_file.clone())));
+}
+
+fn bench_solve_part2(c: &mut Criterion) {
+    let input_file = load_file("data/input.txt");
+    c.bench_function("input", move |b| b.iter(|| solve_part2(input_file.clone())));
+}
+
+criterion_group! {name = benches; config= Criterion::default(); targets= bench_solve, bench_load, bench_solve_part1, bench_solve_part2}
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,30 @@ pub fn solve(input_file: &str) -> (u64, u64) {
     (part_1, part_2)
 }
 
+pub fn load_file(input_file: &str) -> Vec<Vec<char>> {
+    FileParser::new(input_file).parse_grid()
+}
+
+pub fn solve_part1(input_grid: Vec<Vec<char>>) -> u64 {
+    let mut warehouse = PaperHouse::new(input_grid);
+
+    warehouse.remove_available_paper_rolls()
+}
+
+pub fn solve_part2(input_grid: Vec<Vec<char>>) -> u64 {
+    let mut warehouse = PaperHouse::new(input_grid);
+
+    let part_1 = warehouse.remove_available_paper_rolls();
+    let mut paper_rolls_removed_this_go = part_1;
+    let mut part_2 = part_1;
+
+    while paper_rolls_removed_this_go != 0 {
+        paper_rolls_removed_this_go = warehouse.remove_available_paper_rolls();
+        part_2 += paper_rolls_removed_this_go;
+    }
+    part_2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +67,13 @@ mod tests {
         let (part_1, part_2) = solve("data/input.txt");
         assert_eq!(part_1, 1384);
         assert_eq!(part_2, 8013);
+    }
+
+    #[test]
+    fn actual_parts() {
+        let input_file = load_file("data/input.txt");
+        assert_eq!(solve_part1(input_file), 1384);
+        let input_file = load_file("data/input.txt");
+        assert_eq!(solve_part2(input_file), 8013);
     }
 }
